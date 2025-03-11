8d159722e7821e21c803259bd65d8ff7ea17428d
Kunal Kumar Soni <adityaprasad8092@gmail.com>
2025-03-11 14:09:19 +0530

    add login , signup and context folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <StrictMode>
-  <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </StrictMode>
-  </BrowserRouter>,
-)
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
+);
