c9ae0eadb56e1ae91c20496536a86eb3370a924f
Kunal Kumar Soni <adityaprasad8092@gmail.com>
2025-03-11 15:03:28 +0530

    again login ,signup and context folder added

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "./components/Login.jsx";
+import SignUp from "./components/SignUp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </StrictMode>
   </BrowserRouter>
