3a5ca8f790c12164ff4f0a24dc26eec1c7fcbd76
abhaysam2888 <abhaysam2888@gmail.com>
2025-03-12 12:00:56 +0530

    some ui changes

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,44 +1,100 @@
-import React from "react";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [keepLoggedIn, setKeepLoggedIn] = useState(true);
+
   return (
-    <div className="flex flex-col justify-center items-center p-4 min-h-screen">
+    <div className="flex flex-col justify-center items-center px-4 md:px-0 min-h-screen">
       <div className="top-5 left-5 absolute font-bold text-blue-600 text-2xl sm:text-3xl">
         Linked<span className="bg-blue-600 px-1 text-white">in</span>
       </div>
-      <div className="bg-white shadow-lg mt-16 p-6 sm:p-8 rounded-lg w-full max-w-md">
-        <h2 className="mb-4 font-semibold dark:text-gray-700 text-xl sm:text-2xl">
-          Sign in
-        </h2>
-        <div className="mb-4">
-          <label className="block text-gray-700">Email or phone</label>
-          <input
-            type="email"
-            placeholder="Email or phone"
-            className="p-3 border border-gray-300 rounded-lg focus:outline-none w-full dark:placeholder:text-gray-500 dark:text-black"
-          />
-        </div>
-        <div className="relative mb-4">
-          <label className="block text-gray-700">Password</label>
+      <h1 className="mb-10 font-semibold text-gray-800 dark:text-gray-200 text-2xl md:text-3xl text-center mt-5">
+        Make the most of your professional life
+      </h1>
+      <div className="shadow-lg bg-gray-700 p-6 md:p-8 rounded-lg w-full max-w-[455px]">
+        <label className="block text-gray-200">Name</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="mb-3 p-2 border border-gray-300 rounded w-full dark:placeholder:text-gray-500 dark:text-black"
+        />
+        <label className="block text-gray-200">Email or phone number</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="mb-3 p-2 border border-gray-300 rounded w-full dark:placeholder:text-gray-500 dark:text-black"
+        />
+
+        <label className="block text-gray-200">Password</label>
+        <div className="relative">
           <input
             type="password"
-            className="p-3 border border-gray-300 rounded-lg focus:outline-none w-full dark:placeholder:text-gray-500 dark:text-black"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mb-3 p-2 border border-gray-300 rounded w-full dark:placeholder:text-gray-500 dark:text-black"
           />
         </div>
-        <p className="mt-2 text-blue-600 text-sm cursor-pointer">
-          Forgot password?
+        <label className="block text-gray-200">Upload your profile</label>
+        <div className="relative">
+          <input
+            type="file"
+            className="mb-3 p-2 border border-gray-300 rounded w-full dark:placeholder:text-gray-500 dark:text-black"
+          />
+        </div>
+        <label className="block text-gray-200">Upload your cover</label>
+        <div className="relative">
+          <input
+            type="file"
+            className="mb-3 p-2 border border-gray-300 rounded w-full dark:placeholder:text-gray-500 dark:text-black"
+          />
+        </div>
+
+        <div className="flex items-center mb-3">
+          <input
+            type="checkbox"
+            checked={keepLoggedIn}
+            onChange={() => setKeepLoggedIn(!keepLoggedIn)}
+            className="mr-2"
+          />
+          <label className="text-gray-200">Keep me logged in</label>
+        </div>
+        <p className="text-gray-200 text-sm">
+          By clicking Agree & Join or Continue, you agree to the Linkedin
         </p>
-        <button className="bg-blue-600 hover:bg-blue-700 mt-4 p-3 rounded-lg w-full font-semibold text-white">
-          Sign in
+        <p className="mb-3 text-gray-500 text-sm sm:text-center text-start">
+          <a className="font-semibold text-blue-600" href="">
+            User Agreement,{" "}
+          </a>
+          <a className="font-semibold text-blue-600" href="">
+            Privacy Policy,{" "}
+          </a>
+          and{" "}
+          <a className="font-semibold text-blue-600" href="">
+            Cookie Policy.
+          </a>
+        </p>
+        <button className="bg-blue-600 py-2 rounded-3xl w-full font-semibold text-white">
+          Agree & Join
         </button>
+        <div className="my-4 text-gray-500 text-center">or</div>
+        <p className="mt-4 dark:text-gray-200 text-center">
+          Already on LinkedIn?{" "}
+          <a href="#" className="font-semibold text-blue-600">
+            Sign in
+          </a>
+        </p>
       </div>
-      <p className="mt-4 text-gray-600 text-sm sm:text-base">
-        New to LinkedIn?{" "}
-        <span className="text-blue-600 cursor-pointer">Join now</span>
+      <p className="mt-4 text-sm text-center">
+        Looking to create a page for a business?{" "}
+        <a className="font-semibold text-blue-600" href="">
+          Get Help
+        </a>
       </p>
-      <footer className="bottom-4 absolute w-full text-gray-500 text-xs sm:text-sm text-center">
-        LinkedIn © 2025 User Agreement Privacy Policy Cookie Policy
-      </footer>
     </div>
   );
 };
